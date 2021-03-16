6f8e3062eb740028adcf3100d2b6cf95aba8619c
Nikomus <nick.wade@live.co.uk>
2021-03-16 09:40:27 +0000

    feat: new card visual layout

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,108 +1,81 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styled from 'styled-components'
+import { useRef } from 'react'
 
 import { ServicePreview } from '../../pages/index'
-import { VisuallyHidden } from './VisuallyHidden'
+import { VisuallyHidden } from '../Components/VisuallyHidden'
 
 const CardContainer = styled.li`
-  border-radius: 2rem;
+  border-radius: 1rem;
+  border-top-right-radius: 3rem;
   box-shadow: 0 0 9px 2px ${(props) => props.theme.colours.shadow};
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
   margin: 1.5rem 2rem;
   overflow: hidden;
-  padding: 1rem;
   position: relative;
+
+  &:hover {
+    box-shadow: 0 0 15px 7px ${(props) => props.theme.colours.shadow};
+  }
 `
 
 const ServiceName = styled.p`
+  font-family: 'Catamaran', sans-serif;
   font-size: ${(props) => props.theme.fontSizes.small};
-  margin-bottom: 1rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  order: 2;
+  padding: 0 1rem;
 `
 
 const Description = styled.h2`
   align-items: center;
   color: ${(props) => props.theme.colours.purple};
   display: flex;
-  font-size: ${(props) => props.theme.fontSizes.heading};
-  margin-bottom: 1rem;
-  min-height: 4rem;
-  text-transform: capitalize;
-`
-
-const TitleContainer = styled.div`
   font-family: 'Catamaran', sans-serif;
+  font-size: ${(props) => props.theme.fontSizes.heading};
   font-weight: bold;
-  display: flex;
-  flex-direction: column-reverse;
-  width: calc(60% - 1rem);
+  margin-bottom: 1rem;
+  order: 3;
+  padding: 0 1rem;
+  text-transform: capitalize;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    &:focus {
+      outline: 2px dashed ${(props) => props.theme.colours.blue};
+      outline-offset: 2px;
+    }
+  }
 `
 
 const ImageContainer = styled.div`
-  border-bottom-left-radius: 6rem;
-  height: 8rem;
+  order: 1;
+  position: relative;
   overflow: hidden;
-  position: absolute;
-  right: 0;
-  top: 0;
-  width: 40%;
+  height: 10rem;
+  width: 100%;
+  clip-path: url(#cardWave);
 `
 
-const CategoryList = styled.ul`
-  margin-bottom: 1rem;
-`
-
-const Category = styled.li`
-  font-family: 'Catamaran', sans-serif;
-  font-weight: bold;
-  font-size: ${(props) => props.theme.fontSizes.small};
-  display: inline;
-  margin-right: 1rem;
-
-  &:before {
-    content: '';
-    background: ${(props) => props.theme.colours.yellow};
-    border-radius: 50%;
-    display: inline-block;
-    height: 0.8em;
-    margin-right: 0.5rem;
-    width: 0.8em;
-  }
-`
-
-const DetailContainer = styled.div`
-  align-items: flex-end;
+const InfoContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-`
-
-const DetailArea = styled.div`
-  flex: 1 0 60%;
   font-family: 'Lato', sans-serif;
   font-size: ${(props) => props.theme.fontSizes.small};
-`
+  margin: 0 1rem;
+  margin-bottom: 1rem;
+  order: 4;
 
-const ButtonLink = styled.a`
-  align-items: center;
-  background-color: ${(props) => props.theme.colours.purple};
-  border-radius: 5rem;
-  border: 3px solid transparent;
-  color: ${(props) => props.theme.colours.white};
-  display: flex;
-  font-family: 'Catamaran', sans-serif;
-  font-weight: bold;
-  height: 2rem;
-  padding: 0.2rem 2rem;
-  text-decoration: none;
-
-  &:focus {
-    outline: 2px dashed ${(props) => props.theme.colours.blue};
-    outline-offset: 2px;
-  }
-
-  &:hover {
-    background-color: ${(props) => props.theme.colours.purple_light};
-    color: ${(props) => props.theme.colours.purple};
-    transition: 0.3s;
+  div {
+    background-color: #edf7f6;
+    border-radius: 0.5rem;
+    padding: 0.25rem 0.5rem;
+    margin-right: 1rem;
   }
 `
 
@@ -114,23 +87,34 @@ export const Card = ({
   costValue,
   costQualifier,
   age,
-  categories,
 }: ServicePreview): JSX.Element => {
-  const theCats = [
-    categories?.category1 ? categories?.category1 : '',
-    categories?.category2 ? categories?.category2 : '',
-  ].filter(Boolean)
+  const ageDisplay = age ? formatAgeDisplay(age.minAge, age.maxAge) : null
 
-  const theAge = age ? formatAgeDisplay(age.minAge, age.maxAge) : null
-
-  const theCost =
+  const costDisplay =
     costValue || costValue === 0 || costQualifier
       ? formatCostDisplay(costValue, costQualifier)
       : null
 
+  const cardLink = useRef<HTMLAnchorElement | null>(null)
+
   return (
-    <CardContainer>
+    <CardContainer onClick={() => cardLink.current?.click()}>
+      <Description>
+        <Link href={`/service/${id}`} passHref>
+          <a ref={cardLink}>{shortDescription}</a>
+        </Link>
+      </Description>
+
       <ImageContainer>
+        <VisuallyHidden>
+          <svg width="0" height="0">
+            <defs>
+              <clipPath id="cardWave" clipPathUnits="objectBoundingBox">
+                <path d="M 0,1  L 0,0  L 1,0  L 1,0.95  C .75 1.05, .25 .7, 0 0.8 Z" />
+              </clipPath>
+            </defs>
+          </svg>
+        </VisuallyHidden>
         {image?.image && (
           <Image
             src={`/${image.image}`}
@@ -141,30 +125,12 @@ export const Card = ({
         )}
       </ImageContainer>
 
-      <TitleContainer>
-        <Description>{shortDescription}</Description>
-        <ServiceName>{title}</ServiceName>
-      </TitleContainer>
+      <ServiceName>{title}</ServiceName>
 
-      {theCats && (
-        <CategoryList>
-          {theCats.map((cat) => {
-            return <Category key={cat}>{cat}</Category>
-          })}
-        </CategoryList>
-      )}
-
-      <DetailContainer>
-        <DetailArea>
-          {theCost && <p>Cost: {theCost}</p>}
-          {theAge && <p>Age Range: {theAge}</p>}
-        </DetailArea>
-        <Link href={`/service/${id}`} passHref>
-          <ButtonLink>
-            <VisuallyHidden>{`${title} `}</VisuallyHidden> <span>info</span>
-          </ButtonLink>
-        </Link>
-      </DetailContainer>
+      <InfoContainer>
+        {costDisplay && <div>{costDisplay}</div>}
+        {ageDisplay && <div>{ageDisplay}</div>}
+      </InfoContainer>
     </CardContainer>
   )
 }
@@ -175,18 +141,18 @@ export function formatAgeDisplay(
 ): string {
   // There is only a minimum age
   if (!max) {
-    return `${min}+`
+    return `From age ${min}+`
   }
   // There is only a maximum age
   else if (!min) {
-    return `under ${max}`
+    return `Under ${max}s`
   }
   // There is an age range
   else {
     if (min === max) {
-      return `${min}`
+      return `Age ${min}`
     } else {
-      return `${min}-${max}`
+      return `Ages ${min}-${max}`
     }
   }
 }
