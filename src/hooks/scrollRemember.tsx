81863fa92b31ad5d69229e34579239249f086864
James Thistlewood <jamesthistlewood@gmail.com>
2021-03-23 09:55:47 +0000

    fix: forget scroll position when leaving site

--- src/hooks/scrollRemember.tsx
+++ src/hooks/scrollRemember.tsx
@@ -2,29 +2,37 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 interface UseScrollRememberReturns {
-  resumePosition(): void,
+  resumePosition(): void
 }
 
 export const useScrollRemember = (): UseScrollRememberReturns => {
   const router = useRouter()
+  const id = 'lastScroll/' + router.pathname
 
   // Store last scroll position so we can return to it when we load the page next time
   useEffect(() => {
     const handler = () => {
-      localStorage.setItem('lastScroll/' + router.pathname, window.pageYOffset.toString())
+      localStorage.setItem(id, window.pageYOffset.toString())
     }
+
+    const unloadHandler = () => {
+      localStorage.setItem(id, '0')
+    }
+
+    window.addEventListener('beforeunload', unloadHandler)
 
     router.events.on('beforeHistoryChange', handler)
     return () => {
       router.events.off('beforeHistoryChange', handler)
+      window.removeEventListener('beforeunload', unloadHandler)
     }
-  }, [router.events, router.pathname])
+  }, [router.events, id])
 
   const resumePosition = () => {
-    window.scrollTo(0, parseInt(localStorage.getItem('lastScroll/' + router.pathname) || '0'))
+    window.scrollTo(0, parseInt(localStorage.getItem(id) || '0'))
   }
 
   return {
-    resumePosition
+    resumePosition,
   }
 }
