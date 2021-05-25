4a166bfe5a9eb71bbd268b5768a33b4ea5a837bf
edpe <ed@edperkins.co.uk>
2021-05-25 11:32:47 +0100

    feat: add link to manage cookies on privacy policy

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -41,13 +41,67 @@ const Policy = styled.article`
   }
 `
 
+const SubHeading = styled.h3`
+  border-bottom: 1px solid ${(props) => props.theme.colours.yellow};
+  display: inline-block;
+  font-family: 'Catamaran', sans-serif;
+  font-size: ${(props) => props.theme.fontSizes.highlight};
+  margin-bottom: 1rem;
+`
+
+const Text = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.normal};
+  margin-bottom: 1rem;
+  max-width: 40rem;
+  text-align: left;
+`
+
+const CookieButton = styled.a`
+  align-items: center;
+  background-color: ${(props) => props.theme.colours.blue};
+  border-radius: 5rem;
+  border: 3px solid transparent;
+  color: ${(props) => props.theme.colours.white};
+  cursor: pointer;
+  display: flex;
+  font-family: 'Catamaran', sans-serif;
+  font-size: ${(props) => props.theme.fontSizes.highlight};
+  font-weight: bold;
+  justify-content: center;
+  margin-bottom: 1em;
+  margin-top: 0.5em;
+  max-width: 300px;
+  padding: 0.5rem 1.2rem;
+  padding: 0.5rem;
+  position: relative;
+  text-decoration: none;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colours.purple};
+    transition: 0.3s background-color, 0.3s color;
+  }
+
+  &:focus {
+    outline: 2px dashed ${(props) => props.theme.colours.blue};
+    outline-offset: 2px;
+  }
+`
+
 export const PrivacyPage: NextPage<PrivacyPageProps> = ({ content }) => {
   return (
     <Layout>
-      <Content>
+      <Content as="main">
         <VerticalSpacing />
         <Policy dangerouslySetInnerHTML={{ __html: content }} />
         <VerticalSpacing />
+        <SubHeading>Cookies</SubHeading>
+        <Text>
+          Cookies are files saved on your phone, tablet or computer when you
+          visit a website.
+        </Text>
+        <CookieButton href="/cookies">
+          Manage my cookie preferences
+        </CookieButton>
       </Content>
     </Layout>
   )
