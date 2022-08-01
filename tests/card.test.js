c34b1bb5ea032d08b8c5365428e7b2638c1016da
James Gordon <james@neontribe.co.uk>
2022-08-01 12:18:09 +0100

    feat: adding image test logic

--- tests/card.test.js
+++ tests/card.test.js
@@ -11,23 +11,22 @@ import { ThemeProvider } from 'styled-components'
 import { getServices } from '../cms/services'
 
 /*
-* Pulling in the real data to test
-*/
+ * Pulling in the real data to test
+ */
 
-const services = getServices();
+const services = getServices()
 
 /*
-* Test object to see if test fails when a object is missing a value. 
-* Third object is missing shortDescription
-*/
+ * Test object to see if test fails when a object is missing a value.
+ * Third object is missing shortDescription
+ */
 
 it('Card component test', () => {
-
   /*
-  * service - real data
-  * testObj - dummy data
-  * Switch between the two objects above to confirm the test runs correctly
-  */
+   * service - real data
+   * testObj - dummy data
+   * Switch between the two objects above to confirm the test runs correctly
+   */
   services.map((service) => {
     const { container } = render(
       <ThemeProvider theme={MyBestLifeTheme}>
@@ -43,13 +42,14 @@ it('Card component test', () => {
       </ThemeProvider>
     )
 
-    const shortDescription    = container.querySelector('p').textContent;
-    const title               = container.querySelector('h2 a').textContent;
-    const costValue           = container.querySelector('div:nth-of-type(2) > div').textContent;
+    const shortDescription = container.querySelector('p').textContent
+    const title = container.querySelector('h2 a').textContent
+    const costValue = container.querySelector(
+      'div:nth-of-type(2) > div'
+    ).textContent
 
     expect(shortDescription).toBeTruthy()
     expect(title).toBeTruthy()
     expect(costValue).toBeTruthy()
-
   })
 })
--- tests/image.test.js
+++ tests/image.test.js
@@ -7,28 +7,23 @@ import path from 'path'
 import { getServices } from '../cms/services'
 
 /*
-* Pulling all images
-*/
+ * Pulling all images
+ */
 
 const getImages = () => {
+  const contentDirectory = path.join(process.cwd(), './public/img')
+  const fileNames = fs.readdirSync(contentDirectory)
 
-    const contentDirectory = path.join(process.cwd(), './public/img')
-    const fileNames = fs.readdirSync(contentDirectory)
-
-    return fileNames
+  return fileNames
 }
 
-const images = getImages();
-const services = getServices();
+const images = getImages()
+const services = getServices()
 
 it('Services images exist', () => {
-
   services.map((service) => {
-    
-    const imageExists = images.includes( service.image.image.replace('img/', '') );
+    const imageExists = images.includes(service.image.image.replace('img/', ''))
 
-    expect( imageExists ).toBe(true);
-
+    expect(imageExists).toBe(true)
   })
-
 })
